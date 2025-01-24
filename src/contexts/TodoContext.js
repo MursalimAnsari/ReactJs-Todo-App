74fc7bb19ecb29a4317d41ee0ae405b932defefb
MursalimAnsari <mursalim.ansari.1998@gmail.com>
2025-01-24 12:58:18 +0530

    intial commit

---
+++ src/contexts/TodoContext.js
@@ -0,0 +1,26 @@
+import { createContext, useContext } from "react";
+
+
+export const TodoContext = createContext({
+
+    todos: [
+        {
+            id: 1,
+            todo: "todo message",
+            completed: false
+        }
+    ],
+
+    addTodo: (todo)=> {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
+
+
+// create custom hook useTodo
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider
